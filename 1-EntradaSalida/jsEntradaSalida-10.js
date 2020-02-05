4c7bdb7467f85a07ff5746dd65e2925e68f65d5f
Alesena <dicsuresh@gmail.com>
2020-02-05 13:25:49 -0300

    hasta ejercicio 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -3,6 +3,12 @@ Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
-{
+{var numero1;
+    var numero2;
+    var descuento;
+    parseInt(descuento = 1.25);
+    numero1 = parseInt(document.getElementById("importe").value);
+    numero2 = (numero1 / descuento);
+    document.getElementById("resultado").value = numero2
 	
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -3,7 +3,15 @@ transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
-{
+{ var numero1
+    var numero2
+    var aumento
+    parseInt(aumento = 1.10);
+    numero1 = parseInt(document.getElementById("sueldo").value);
+    numero2 = numero1*aumento;
+    document.getElementById("resultado").value = numero2;
+    
+    
 
-	
+    
 }
